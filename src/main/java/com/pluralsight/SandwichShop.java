1f1279c17071256b0c1db0834219e845cc690f30
Qi Qing <learn.qiqing@gmail.com>
2025-09-25 14:29:16 -0400

    add print for age and input, add size variables.

--- src/main/java/com/pluralsight/SandwichShop.java
+++ src/main/java/com/pluralsight/SandwichShop.java
@@ -14,25 +14,37 @@ public class SandwichShop {
         //Prompt the user for the size of the sandwich (1 or 2):
         // a. 1: Regular: base price $5.45
         // b. 2: Large: base price $8.95
-        //ask for input
+        //ask for size input
 
         System.out.println("What size of sandwich would you like?");
         System.out.println("1: Regular: base price $5.45");
         System.out.println("2: Large: base price $8.95");
         System.out.println("Please enter 1 or 2");
-        int input = userInput.nextInt();
+        int sizeInput = userInput.nextInt();
 
-
-
-
-
-
-
+        float regular = 5.45f;
+        float large = 8.95f;
 
         //Prompt the user for their age:
         //a. Student (17 years old or younger) — receive a 10% discount
         //b. Seniors (65 years old or older) — receive a 20% discount
         //Display the cost of the sandwich to the screen
+        //ask for age.
+
+        System.out.println("What is your age?");
+        int ageInput = userInput.nextInt();
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
